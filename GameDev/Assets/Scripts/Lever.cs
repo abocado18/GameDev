147f91c458c352395a10d3759edbcb99978e0ce5
abocado18 <40613453+abocado18@users.noreply.github.com>
2021-12-08 16:52:34 +0100

    Improved Lever

--- GameDev/Assets/Scripts/Lever.cs
+++ GameDev/Assets/Scripts/Lever.cs
@@ -6,6 +6,7 @@ public class Lever : MonoBehaviour
 {
     bool activated;
     bool canChange;
+    bool canInteract;
     public GameObject door;
     public Transform lever;
 
@@ -15,23 +16,23 @@ public class Lever : MonoBehaviour
         canChange = true;
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void Update()
     {
-        if(collision.gameObject.tag == "Player")
+        if(canInteract)
         {
-            if(Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                if(!activated && canChange)
+                if (!activated && canChange)
                 {
                     door.SetActive(false);
                     activated = true;
                     canChange = false;
                     lever.rotation = Quaternion.Euler(0f, 0f, -30.48f);
                     Invoke("SetChangeTrue", 1f);
-                    
+
                 }
 
-                else if(activated && canChange)
+                else if (activated && canChange)
                 {
                     door.SetActive(true);
                     activated = false;
@@ -39,9 +40,34 @@ public class Lever : MonoBehaviour
                     lever.rotation = Quaternion.Euler(0f, 0f, 30.48f);
                     Invoke("SetChangeTrue", 1f);
                 }
-               
+
             }
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            canInteract = false;
+        }
+        
     }
 
     private void SetChangeTrue()
