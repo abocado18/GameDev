d90e0f3f7c83af0b991d13a6364e9a5d448c95a8
Susana Reyes <60724497+susi1603@users.noreply.github.com>
2022-01-11 19:52:59 +0100

    testing modularity

---
+++ GameDev/Assets/Scripts/switchCharacter.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchCharacter : MonoBehaviour
+{
+    public GameObject avatar1, avatar2;
+    int whichAvatarIsOn = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        avatar1.gameObject.SetActive(true);
+        avatar2.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void SwitchAvatar()
+    {
+        switch (whichAvatarIsOn)
+        {
+            case 1:
+                whichAvatarIsOn = 2;
+
+                avatar1.gameObject.SetActive(false);
+                avatar2.gameObject.SetActive(true);
+                break;
+            case 2:
+                whichAvatarIsOn = 1;
+
+                avatar1.gameObject.SetActive(true);
+                avatar2.gameObject.SetActive(false);
+                break;
+
+        }
+    }
+}
